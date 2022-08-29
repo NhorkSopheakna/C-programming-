d7b22d1b04c36f09c23163f8fd5509460b243bca
NhorkSopheakna <sopheaknanhork@gmail.com>
2022-08-29 16:53:29 +0700

    Done

---
+++ Week8/N15.c
@@ -0,0 +1,23 @@
+ឈាង ឆាយអុី- Chheang Chhayi
+15 #include <stdio.h>
+
+int main(){
+    
+    int computer_num,usernum;
+    computer_num = 50;
+    // 
+    while(1){
+      
+        printf("Enter your guess : ");
+        scanf("%d",&usernum);
+
+        if (computer_num != usernum){
+            printf("have another guess");
+      
+        // 
+        }else if(computer_num == usernum){
+            printf("Well done, you took [count] attempts %d",usernum);  
+    }
+
+    return 0;
+}
