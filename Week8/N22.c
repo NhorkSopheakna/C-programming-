6308355694910053f99824978356973b89e65fd5
NhorkSopheakna <sopheaknanhork@gmail.com>
2022-08-29 16:52:19 +0700

    Done

---
+++ Week8/N22.c
@@ -0,0 +1,22 @@
+> ឈឿន បញ្ញា- Chhoeun Panha:
+#include<stdio.h>
+ int main()
+ {
+     int i, j, sum; //Variable
+
+     printf("Perfect numbers in the given range are: \n");
+
+     for( i=1; i<=10000; i++) // Check number from 1 to 10000
+     {
+         sum=0;
+         for( j=1; j<=i/2; j++) //j is depend on i
+         {
+             if(i%j==0)  //The remain from i /j equal 0
+                 sum+=j; //sum=sum+j
+         }
+         if(i==sum) printf("%d\t",i); //The result will display when i=sum
+     }
+
+     return 0;
+ }
+
