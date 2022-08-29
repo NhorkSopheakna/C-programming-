57df80f2bd3fa7c20a2fc7e6813f530fe22f8930
NhorkSopheakna <sopheaknanhork@gmail.com>
2022-08-29 17:04:38 +0700

    Done

---
+++ Week7.c/N3.7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int age;
+       printf("enter the age : ");
+       scanf ("%d", &age);
+    if(age > 16){
+       printf ("ticketPrice = 20") ;
+    }
+    else {
+       printf ("ticketPrice = 10") ;
+    }
+}
